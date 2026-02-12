758404550d72bb4c0218446575f592fb081ba422
Carlos Acosta <carlosacta.dev@gmail.com>
2026-02-12 12:07:25 -0500

    Add remove duplicates

--- doubly_linked_list.rb
+++ doubly_linked_list.rb
@@ -65,6 +65,20 @@ class DoublyLinkedList
     end
   end
 
+  def remove_duplicates
+    seen = {}
+    current = @head
+    while current
+      if seen[current.data]
+        # Remove the duplicate node
+        remove_by_value(current.data)
+      else
+        seen[current.data] = true
+      end
+      current = current.next
+    end
+  end
+
   # return the value and the index of the node if found, otherwise return nil
   def search(value)
     current = @head
@@ -76,6 +90,16 @@ class DoublyLinkedList
     end
     nil
   end
+
+  def size
+    count = 0
+    current = @head
+    while current
+      count += 1
+      current = current.next
+    end
+    count
+  end
 end
 
 # Test the Node class
@@ -83,13 +107,23 @@ list = DoublyLinkedList.new
 list.append(10)
 list.append(20)
 list.prepend(5)
+puts "List size: #{list.size}" # Should be 3
 puts "Node 1 data: #{list.tail.data.inspect}"
 puts "Node 2 data: #{list.head.data.inspect}"
 puts "Node 3 data: #{list.head.next.data.inspect}"
 
 list.remove_by_value(20)
 puts "After removing 20, tail data: #{list.tail.data.inspect}" # Should be 10 since 20 was the tail
+puts "List size after removal: #{list.size}" # Should be 2
 
 search_result = list.search(10)
 # Should find 10
-puts "Search for 10: #{search_result[:value].inspect}, at index: #{search_result[:index]}" 
+puts "Search for 10: #{search_result[:value].inspect}, at index: #{search_result[:index]}"
+
+search_result = list.search(20)
+# Should not find 20
+puts "Search for 20: #{search_result.inspect}"
+
+list.append(5)
+list.remove_duplicates
+puts "After removing duplicates, list size: #{list.size}" # Should be 2 since there are two 5s
